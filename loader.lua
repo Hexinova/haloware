d8696ed08929579306dbac3ee8b4a667bc8218b6
Hexinova <130008390+Hexinova@users.noreply.github.com>
2025-08-19 08:44:25 -0400

    Coded base for loader, might improve later

--- loader.lua
+++ loader.lua
@@ -1,1 +1,5 @@
--- uhh put a loader here
+local PlaceID = game.PlaceId
+
+if PlaceID == 142823291 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Hexinova/nefware/main/mm2.lua"))()
+end
